a8fe58411372e01dd735aeea89eeaffaa765095e
ndhieunguyen <hieunguyen.kientuong@gmail.com>
2024-08-21 23:17:21 +0700

    feat: update split symbol

--- main.js
+++ main.js
@@ -6,15 +6,23 @@ document.getElementById('start').addEventListener('click', () => {
         function extractQuestionsAndAnswers(element){
             try{
                 let question = element.children[0].children[0].children[0].children[0].innerText;
-                let answer = element.children[1].children[0].children[0].children[0].textContent;
+                let answer = element.children[0].children[0].children[0].children[0].children[1].children[0].children[0].children[0].textContent
     
-                let questionAndOptions = question.split("\n\n");
+                let questionAndOptions = question.split("\n");
                 question = questionAndOptions[0]
-                options = questionAndOptions[1]
-                questionAndOptions = question + "\n" + options
+                
+                let i = 1;
+                let options = [];
+                for (i; i < questionAndOptions.length-1; i++) {
+                    options.push(questionAndOptions[i])
+                }
+
+
+                console.log("question: " + question)
+                console.log("options: " + options.join('\n'))
+                questionAndOptions = question + "\n" + options.join('\n')
     
-                // return [questionAndOptions, answer].join("~")
-                return [questionAndOptions, answer].join("\n")
+                return [questionAndOptions, answer].join("~~")
             }
             catch{
                 console.log(element);
@@ -22,20 +30,19 @@ document.getElementById('start').addEventListener('click', () => {
             }
         }
 
-        let viewMore = document.getElementsByClassName("AssemblyButtonBase AssemblyPrimaryButton--default AssemblyButtonBase--medium AssemblyButtonBase--padding AssemblyButtonBase--fullWidth")
+        let viewMore = document.getElementsByClassName("AssemblyButtonBase AssemblyTextButton AssemblyTextButton--secondary AssemblyButtonBase--large AssemblyButtonBase--padding")
         if (viewMore.length > 0){
             viewMore[0].click()
         }
         let info = [];
-        const allQuestionsAndAnswers = document.getElementsByClassName("SetPageTerm-content");
+        const allQuestionsAndAnswers = document.getElementsByClassName("SetPageTerms-term");
         
         for (ele of allQuestionsAndAnswers){
             info.push(extractQuestionsAndAnswers(ele));
         }
 
         const link = document.createElement("a");
-        // const file = new Blob([info.join("#")], { type: 'text/plain' });
-        const file = new Blob([info.join("\n\n")], { type: 'text/plain' });
+        const file = new Blob([info.join("##")], { type: 'text/plain' });
 
         link.href = URL.createObjectURL(file);
         link.download = "questions.txt";
